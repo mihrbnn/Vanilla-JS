ca8da15ccfbb57b01b3b1a9491e54757fb3e636e
Mihriban <mihkilickan@gmail.com>
2022-07-05 23:56:01 +0300

    exercise added

---
+++ 09- Hold Shift and Check Checkbox/script.js
@@ -0,0 +1,26 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+  console.log("this",lastChecked)
+
+}
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);
