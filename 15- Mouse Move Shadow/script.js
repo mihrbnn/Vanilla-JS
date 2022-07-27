7308f66e39bbdf9dcca28d27e4ebdda11992e10e
Mihriban <mihkilickan@gmail.com>
2022-07-27 22:47:30 +0300

    css add

--- 15- Mouse Move Shadow/script.js
+++ 15- Mouse Move Shadow/script.js
@@ -1,11 +1,23 @@
-const hero = document.querySelector(".hero")
-const text = hero.querySelector("h1")
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 500
 
-function shadow(e){
+function shadow(e) {
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
 
-    const {offsetWidth:width,offsetHeight:height} = hero
+  if (this != e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
 
-    console.log(e)
+  const xWalk = Math.round((x / width) * walk - walk / 2);
+  const yWalk = Math.round((y / height) * walk - walk / 2);
+
+  text.style.textShadow = `${xWalk}px ${yWalk}px rgba(255,0,255,0.7),
+  ${xWalk * -1}px ${yWalk}px rgba(0,255,255,0.7),
+  ${yWalk}px ${xWalk * -1}px rgba(0,255,0,0.7),
+  ${yWalk * -1}px ${xWalk}px rgba(0,0,255,0.7)`;
 }
 
-hero.addEventListener('mousemove', shadow);
+hero.addEventListener("mousemove", shadow);
